e17a648fb9d733791b844fed205bff6fe2feff0b
Andrey Lapaev <boss.lapaev01@mail.ru>
2024-07-30 13:23:58 +0300

    Inital

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-4">
+    <a href="#">
+      <img src="./assets/img/logo.png" alt="Logo" class="h-9" />
+    </a>
+    <a href="#">
+      <div>Day complete!</div>
+    </a>
+  </header>
+  <main>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero animi ducimus voluptas atque qui
+    saepe laudantium vero, voluptatibus reiciendis, ex repudiandae soluta ratione maiores quos
+    quaerat dignissimos illum odio doloremque optio incidunt. Quo inventore nemo quaerat molestias
+    voluptatem voluptatum doloremque quis totam eaque sequi exercitationem quasi vero illo velit
+    culpa aperiam, odit qui facilis ut maxime officiis? Atque officiis aut rerum dignissimos sunt,
+    perferendis possimus nobis quod quae debitis, quas provident aperiam est perspiciatis commodi
+    hic consectetur praesentium! Ullam vel, non mollitia ratione eaque minima voluptas! Neque,
+    obcaecati commodi. Id amet saepe optio nam hic architecto nobis sapiente cum. Quas.
+  </main>
+</template>
