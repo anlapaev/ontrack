691520f69dcbc9e5fe4ce13b8c1c041fdd37d1c6
Andrey Lapaev <boss.lapaev01@mail.ru>
2024-07-31 14:42:59 +0300

    11. Clickable link in the header

--- src/App.vue
+++ src/App.vue
@@ -24,14 +24,21 @@ onMounted(() => {
         currentPage.value = normalizePageHash()
     })
 })
+
+function goTo(page) {
+    currentPage.value = page
+}
 </script>
 
 <template>
-    <TheHeader />
+    <TheHeader
+        @go-to-timeline="goTo(PAGE_TIMELINE)"
+        @go-to-progress="goTo(PAGE_PROGRESS)"
+    />
     <main class="flex flex-grow flex-col">
         <TheTimeline v-show="currentPage === PAGE_TIMELINE" />
         <TheActivities v-show="currentPage === PAGE_ACTIVITIES" />
         <TheProgress v-show="currentPage === PAGE_PROGRESS" />
     </main>
-    <TheNav :current-page="currentPage" @navigate="currentPage = $event" />
+    <TheNav :current-page="currentPage" @navigate="goTo($event)" />
 </template>
--- src/components/TheHeaderProgress.vue
+++ src/components/TheHeaderProgress.vue
@@ -1,9 +1,10 @@
 <script setup>
 import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+import { PAGE_PROGRESS } from '@/constans';
 </script>
 
 <template>
-    <a href="#" class="text-sm font-medium text-gray-500">
+    <a :href="`#${PAGE_PROGRESS}`" class="text-sm font-medium text-gray-500">
         <div v-if="false" class="flex items-center gap-1">
             Day complete!
             <CheckCircleIcon class="h-7 text-green-500" />
