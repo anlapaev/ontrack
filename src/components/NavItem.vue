c9209adacf4c4833a72c0375988c318fc19d05c9
Andrey Lapaev <boss.lapaev01@mail.ru>
2024-07-31 09:54:30 +0300

    9. Backlighting of the active menu item

--- src/components/NavItem.vue
+++ src/components/NavItem.vue
@@ -6,7 +6,7 @@ export default {
 
 <template>
     <li  class="flex-1">
-        <a  class="flex flex-col items-center p-2 text-xs capitalize">
+        <a v-bind="$attrs" class="flex flex-col items-center p-2 text-xs capitalize">
             <slot></slot>
         </a>
     </li>
