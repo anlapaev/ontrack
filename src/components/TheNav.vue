ba328dd6b1d300daeb4c0d305c88f1aded04e279
Andrey Lapaev <boss.lapaev01@mail.ru>
2024-07-31 09:25:08 +0300

    7. Inheritance of attributes

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+import NavItem from '@/components/NavItem.vue'
 
 const navItems = {
     timeline: ClockIcon,
@@ -11,18 +12,9 @@ const navItems = {
 <template>
     <nav class="sticky bottom-0 z-10 bg-white">
         <ul class="flex items-center justify-around border-t">
-            <li v-for="(icon, page) in navItems" :key="page" class="flex-1">
-                <a
-                    v-bind:href="`#${page}`"
-                    class="flex flex-col items-center p-2 text-xs capitalize"
-                >
-                    <component :is="icon" class="h-6 w-6" /> {{ page }}
-                    <!-- <ClockIcon v-if="page === 'timeline'" class="h-6 w-6" />
-                    <ListBulletIcon v-else-if="page === 'activities'" class="h-6 w-6" />
-                    <ChartBarIcon v-else class="h-6 w-6" /> -->
-                    <!-- {{ page }} -->
-                </a>
-            </li>
+            <NavItem v-for="(icon, page) in navItems" :key="page" :href="`#${page}`">
+                <component :is="icon" class="h-6 w-6" /> {{ page }}
+            </NavItem>
         </ul>
     </nav>
 </template>
