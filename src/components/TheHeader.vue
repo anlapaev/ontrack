d08b3ebb09fb2389009b4e6d242e76799320f9be
Andrey Lapaev <boss.lapaev01@mail.ru>
2024-07-31 09:16:56 +0300

    6. Header and navigation components

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,13 @@
+<script setup>
+import TheLogo from '@/components/TheLogo.vue'
+import TheHeaderProgress from '@/components/TheHeaderProgress.vue'
+</script>
+
+<template>
+    <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-4">
+        <TheLogo />
+        <TheHeaderProgress />
+    </header>
+</template>
+
+<style lang="scss" scoped></style>
